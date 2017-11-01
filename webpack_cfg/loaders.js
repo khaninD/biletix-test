6ed90f7d4f4b3a55930fc071a0eebdad5bae4c07
khanin_daniil <khanin_daniil@mail.ru>
2017-11-01 19:04:24 +0300

    init

---
+++ webpack_cfg/loaders.js
@@ -0,0 +1,94 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = () => {
+  return {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            {
+              laoder: 'postcss-loader'
+            }
+          ],
+          publicPath: '/'
+        })
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            {
+              laoder: 'postcss-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ],
+          publicPath: '/'
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            {
+              laoder: 'postcss-loader'
+            },
+            {
+              loader: 'less-loader'
+            }
+          ],
+          publicPath: '/'
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './img/[name].[ext]?[hash:12]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './fonts/[name].[ext]?[hash:12]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
